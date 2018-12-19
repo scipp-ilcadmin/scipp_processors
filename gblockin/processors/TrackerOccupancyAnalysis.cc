febd4fdb916f4bc0b115482643245750365a80e9
ilcadmin <ilcadmin@atlas04.ucsc.edu>
2018-12-19 00:00:05 -0800

    autocommit on Wed Dec 19 00:00:05 PST 2018

--- gblockin/processors/TrackerOccupancyAnalysis.cc
+++ gblockin/processors/TrackerOccupancyAnalysis.cc
@@ -35,29 +35,9 @@ TrackerOccupancyAnalysis TrackerOccupancyAnalysis;
 
 
 static TFile* _rootfile;
-static TH1D* _tileHits;
-static TH1I* _layers;
-static TH1D* _xPos;
-static TH1D* _yPos;
 static TH2D* _xyPos;
 static TH3D* _xyzPos;
-static TH1D* _eDep;
-static TH1D* _cells;
-static TH1F* _xmom;
-static TH1F* _ymom;
-static TH1F* _zmom;
-static TH2F* _xymom, *_yzmom, *_xzmom;
-static TH3F* _xyzmom;
 static int _nEvt = 0;
-static double pixelSize = 30.0; // Pixel size is in microns
-static double tileSize; // Tile size also in microns
-static double brlLayerArea[5] = {14817.6, 21168, 31752, 42336, 52920};
-static int tileNumBrl[5];
-int shingleError = 0;
-int brlLyrN = 5;
-int bunchCrossigns = 1;
-bool aligned;
-static vector< map<string,long> > BrlTiles;
 
 static vector<int> layers;
 static vector<int> subDets;
@@ -98,30 +78,9 @@ void TrackerOccupancyAnalysis::init()
   streamlog_out(DEBUG) << " init called " << endl;
   cout << "Initialized "  << endl;
   _rootfile = new TFile("TOA.root", "RECREATE");
-  _tileHits = new TH1D("BrlEventsVCsHit", "BrlEventsVCsHit", 57, 997500, 1002000);
-  _xPos = new TH1D("xposhits", "xposhits", 57, -62.0, 55.0);
-  _yPos = new TH1D("yposhits", "yposhits", 57, -41.0, 48.0);
   _xyPos = new TH2D("xypos", "xypos", 57, -100.0, 100.0, 57, -100.0, 100.0);
-  _xyzPos = new TH3D("xyzpos", "xyzpos", 57, -100.0, 100.0, 57, -100.0, 100.0, 57, -100.0, 100.0);
-  _layers = new TH1I("layers", "layers", 57, -1, 7);
-  _eDep = new TH1D("energy", "energy", 57, -1000, 1000);
-  _xmom = new TH1F("xmom", "xmom", 57, -85.0, 85.0);
-  _ymom = new TH1F("ymom", "ymom", 57, -85.0, 85.0);
-  _zmom = new TH1F("zmom", "zmom", 57, -85.0, 85.0);
-  _xymom = new TH2F("xymom", "xymom", 57, -85, 85, 57, -85, 85);
-  _xyzmom = new TH3F("xyzmom", "xyzmom", 57, -85, 85, 57, -85, 85, 57, -85, 85);
+  _xyzPos = new TH3D("xyzpos", "xyzpos", 57, -100.0, 100.0, 57, -100.0, 100.0, 57, -185.0, 185.0);
   _nEvt = 0;
-  tileSize = pixelSize/1000.0; // No clue why this is set like this
-                               // It just is
-                               // Deal with it
-  for (int i=0; i<brlLyrN; ++i)
-    {
-      BrlTiles.push_back(map<string,long>());
-    }
-  for (int i=0; i<brlLyrN; ++i)
-    {
-      tileNumBrl[i] = (int)(brlLayerArea[i]/(tileSize * tileSize));
-    }
 
 }
 
@@ -133,23 +92,12 @@ void TrackerOccupancyAnalysis::processRunHeader( LCRunHeader* run)
 void TrackerOccupancyAnalysis::processEvent( LCEvent * evt)
 {
   _nEvt++;
-  int check_layer = 0;
-  int hit_count_limit = 100;
-  bool use_limit = false;
-  bool reject_negative = false;
-  int hit_count = 0;
-  double brlXseg = tileSize;
-  double brlYseg = tileSize;
-  int offsetX = 500000;
-  int offsetY = 500000;
-  LCCollection* barrelHits = evt->getCollection("SiVertexBarrelHits");
-  //LCCollection* endcapHits = evt->getCollection("SiVertexEndcapHuts"); will do endcaps after barrelHits
-
-  _nEvt++;
-  for (int i = 0; i < barrelHits->getNumberOfElements(); ++i)
+  //LCCollection* barrelHits = evt->getCollection("SiVertexBarrelHits");
+  LCCollection* endcapHits = evt->getCollection("SiVertexEndcapHits");
+  for (int i = 0; i < endcapHits->getNumberOfElements(); ++i)
     {
-      SimTrackerHit* hit = dynamic_cast<SimTrackerHit*>(barrelHits->getElementAt(i));
-      CellIDDecoder<SimTrackerHit> idDec( barrelHits );
+      SimTrackerHit* hit = dynamic_cast<SimTrackerHit*>(endcapHits->getElementAt(i));
+      CellIDDecoder<SimTrackerHit> idDec( endcapHits );
       int layer = idDec( hit )[ILDCellID0::layer];
       layers.push_back(layer);
       int subdet = idDec( hit )[ILDCellID0::subdet];
@@ -178,22 +126,12 @@ void TrackerOccupancyAnalysis::processEvent( LCEvent * evt)
       ymomVals.push_back(ymom);
       float zmom = hit->getMomentum()[2];
       zmomVals.push_back(zmom);
-      int IDx = (int) ((posx/brlXseg) + 500000);
-      int IDy = (int) ((posy/brlYseg) + 500000);
-      int tileID = (module + IDx + IDy);
-      tileIDVals.push_back(tileID);
-      _tileHits->Fill(tileID);
-      _xPos->Fill(posx);
-      _yPos->Fill(posy);
-      _xyPos->Fill(posx, posy);
-      _xyzPos->Fill(posx, posy, posz);
-      _layers->Fill(layer);
-      _eDep->Fill(eDep);
-      _xmom->Fill(xmom);
-      _ymom->Fill(ymom);
-      _zmom->Fill(zmom);
-      _xymom->Fill(xmom, ymom);
-      _xyzmom->Fill(xmom, ymom, zmom);
+      if (side == 1) 
+	{
+	  //cout << module << endl;
+	  _xyPos->Fill(posx, posy);
+	  _xyzPos->Fill(posx, posy, posz);
+	}
     }
   }
 
@@ -205,9 +143,9 @@ void TrackerOccupancyAnalysis::check( LCEvent * evt)
 
 void TrackerOccupancyAnalysis::end()
 {
-  cout << " max energy: " << getMax(eDepVals) << " MinEnergy: " << getMin(eDepVals) << endl;
-  cout << " max xmom: " << getMax(xmomVals) << " min xmom: " << getMin(xmomVals) << endl;
-  cout << " max ymom: " << getMax(ymomVals) << " min ymom: " <<getMin(ymomVals) << endl;
-  cout << " max zmom: " << getMax(xmomVals) << " min zmom: " <<getMin(zmomVals) << endl;
+  //cout << " max energy: " << getMax(eDepVals) << " MinEnergy: " << getMin(eDepVals) << endl;
+  //cout << " max xmom: " << getMax(xmomVals) << " min xmom: " << getMin(xmomVals) << endl;
+  //cout << " max ymom: " << getMax(ymomVals) << " min ymom: " <<getMin(ymomVals) << endl;
+  //cout << " max zmom: " << getMax(xmomVals) << " min zmom: " <<getMin(zmomVals) << endl;
   _rootfile->Write();
 }
