0ccca49b1af9606a353c159e1abfa706fad642b6
ilcadmin <ilcadmin@atlas04.ucsc.edu>
2019-04-14 00:00:05 -0700

    autocommit on Sun Apr 14 00:00:05 PDT 2019

--- gblockin/processors/example.cc
+++ gblockin/processors/example.cc
@@ -18,13 +18,21 @@
 #include "example.h"
 #include "scipp_ilc_utilities.h"
 #include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <algorithm>
 
+#include <UTIL/ILDConf.h>
 #include <EVENT/LCCollection.h>
 #include <EVENT/SimCalorimeterHit.h>
+#include <EVENT/SimTrackerHit.h>
 #include <EVENT/MCParticle.h>
 
 #include <TFile.h>
 #include <TH2D.h>
+#include <TH3D.h>
+#include <TGraph2D.h>
+
 
 // ----- include for verbosity dependend logging ---------
 #include "marlin/VerbosityLevels.h"
@@ -38,59 +46,73 @@ using namespace std;
 example example;
 
 static TFile* _rootfile;
-static TH2F* _plot;
-static TH1F* _histo;
-
+static TH2D* mods;
+static TH3D* threedim;
+static vector<string> vec;
 static int _nEvt=0;
 
 
-example::example() : Processor("example") {
+example::example() : Processor("example") 
+{
     // modify processor description
     _description = "Protype Processor" ;
-
-    // register steering parameters: name, description, class-variable, default value
-    registerInputCollection( LCIO::MCPARTICLE, "CollectionName" , "Name of the MCParticle collection"  , _colName , std::string("MCParticle") );
-    
-    registerProcessorParameter( "RootOutputName" , "output file"  , _root_file_name , std::string("output.root") );
 }
 
 
-void example::init() { 
+void example::init()
+{ 
     streamlog_out(DEBUG) << "   init called  " << std::endl ;
-    cout << "Initialized " << endl;
-
-    _rootfile = new TFile("example.root","RECREATE");
-
-    _plot = new TH2F("hh", "Hit-Hit HeatMap", 300.0, -150.0, 150.0, 300.0, -150.0, 150.0);
-    _histo = new TH1F("energy", "Energy Distribution", 300, 0, 550);
-
+    _rootfile = new TFile("stahp.root","RECREATE");
+    mods = new TH2D("mods", "mods", 1000, -100, 100, 1000, -100, 100);
+    threedim = new TH3D("threedim", "3-D model", 100, -50, 50, 100, -50, 50, 100, -150, 150);
     _nEvt = 0 ;
 }
 
 
 
-void example::processRunHeader( LCRunHeader* run) { 
-//    _nRun++ ;
+void example::processRunHeader( LCRunHeader* run)
+{ 
 } 
+
+
 void example::processEvent( LCEvent * evt ) { 
-    LCCollection* col = evt->getCollection( _colName );
+    LCCollection* barrelhits = evt->getCollection( "SiVertexBarrelHits" );
     _nEvt++;
-    for(int i=0; i < col->getNumberOfElements(); ++i){
-      MCParticle* particle=dynamic_cast<MCParticle*>(col->getElementAt(i));
-      int pid=particle->getPDG();
-      //WRITE CODE HERE
-    }
+    for(int i=0; i < barrelhits->getNumberOfElements(); ++i)
+      {
+	SimTrackerHit* hit = dynamic_cast<SimTrackerHit*>(barrelhits->getElementAt(i));
+	CellIDDecoder<SimTrackerHit> idDec(barrelhits);
+	int layer = idDec( hit )[ILDCellID0::layer];
+	int side = idDec ( hit )[ILDCellID0::side];
+	int module = idDec (hit)[ILDCellID0::module];
+	int sensor = idDec (hit)[ILDCellID0::sensor];
+	int subdet = idDec (hit)[ILDCellID0::subdet];
+	string all = to_string(layer) + ", " + to_string(module) + ", " + to_string(sensor) + ", " + to_string(subdet);
+	double posx = hit->getPosition()[0];
+	double posy = hit->getPosition()[1];
+	double posz = hit->getPosition()[2];
+	mods->Fill(posx,posy);
+	if (layer ==1 && module ==1)
+	  {
+	    threedim->Fill(posx, posy, posz); 
+	  }
+	//	if(std::find(vec.begin(), vec.end(), subdet) == vec.end())
+	//  {
+	//    vec.push_back(subdet);
+	//  }
+      }
 }
 
 
-void example::check( LCEvent * evt ) { 
+void example::check( LCEvent * evt )
+{ 
     // nothing to check here - could be used to fill checkplots in reconstruction processor
 }
 
 
 
-void example::end(){ 
+void example::end()
+{ 
   cout << "number of events: " << _nEvt << endl;
-  
   _rootfile->Write();
 }
