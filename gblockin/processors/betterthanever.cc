9a8f8670ab9170560f0f5eed10b971c5d5c9444c
ilcadmin <ilcadmin@atlas04.ucsc.edu>
2019-05-20 00:00:04 -0700

    autocommit on Mon May 20 00:00:04 PDT 2019

--- gblockin/processors/betterthanever.cc
+++ gblockin/processors/betterthanever.cc
@@ -57,6 +57,8 @@ static int nhit;
 static Barrel barrel{};
 static double rsuba[5][30][2] = {{{0.0}}};
 static double thetas[5][30][1];
+static int modhits[5][30] = {{0}};
+static int layerhits[5] = {0};
 static TH1D* posmomzvals;
 static TH1D* negmomzvals;
 static TH1D* phigone[5];
@@ -66,8 +68,12 @@ static TH1D* phigonepos[5];
 static vector<double> vecids;
 static TH2D* newmods[5][30];
 static TH2D* ogxyplane;
+static TH2D* trimmedogplane;
 static TH2D* newxyplane;
 static vector<int> particleids;
+static TH1D* zgone[5];
+static double avgradvals[5] = {0};
+static int nhitlayer[5] = {0};
 
 template<typename T>
 static T getMax(vector<T> &vec) //this gets the greatest value in a vector
@@ -91,27 +97,28 @@ void betterthanever::init()
 { 
     streamlog_out(DEBUG) << "   init called  " << std::endl ;
     _rootfile = new TFile("first.root","RECREATE");  //Any histograms would be created in this block
-    barrel.emplace_back(12, PixelGrid(10, vector<int>(126,0)));
-    barrel.emplace_back(12, PixelGrid(14, vector<int>(126,0)));
-    barrel.emplace_back(18, PixelGrid(14, vector<int>(126,0)));
-    barrel.emplace_back(24, PixelGrid(14, vector<int>(126,0)));
-    barrel.emplace_back(30, PixelGrid(14, vector<int>(126,0)));    
+    //barrel.emplace_back(12, PixelGrid(10, vector<int>(126,0)));
+    //barrel.emplace_back(12, PixelGrid(14, vector<int>(126,0)));
+    //barrel.emplace_back(18, PixelGrid(14, vector<int>(126,0)));
+    //barrel.emplace_back(24, PixelGrid(14, vector<int>(126,0)));
+    //barrel.emplace_back(30, PixelGrid(14, vector<int>(126,0)));    
     ogxyplane = new TH2D("ogxyplane", "L", 500, -100, 100, 500, -100, 100);
-    newxyplane = new TH2D("newxyplane", "L", 500, -100, 100, 500, -100, 100);
-    posmomzvals = new TH1D("posmomzvals", "Z momentum values; momentum in GeV", 50, .18, .5);
-    negmomzvals = new TH1D("negmomzvals", "Z momentum values; momentum in GeV", 50, -.5, -.18);
-    for (int i=0; i < 5; ++i)
-      {
-	phigone[i] = new TH1D(Form("phigone%d", i+1), "Collapsed in Phi; Z value of hit", 126, -64, 64);
-	phigoneneg[i] = new TH1D(Form("phigoneneg%d", i+1), "Collapsed in Phi, Lesser Vals", 42, -65, -21);
-	phigonemid[i] = new TH1D(Form("phigonemid%d", i+1), "Collapsed in Phi, Middle Vals", 42,  -21, 21);
-	phigonepos[i] = new TH1D(Form("phigonepos%d", i+1), "Collapsed in Phi, Higher Vals;", 42,   21 ,65);
-	
+    trimmedogplane = new TH2D("trimmedogplane", "L", 500, -70, 70, 500, -70, 70);
+    newxyplane = new TH2D("newxyplane", "L", 500, -30, 30, 500, -100, 100);
+    //posmomzvals = new TH1D("posmomzvals", "Z momentum values; momentum in GeV", 50, .18, .5);
+    //negmomzvals = new TH1D("negmomzvals", "Z momentum values; momentum in GeV", 50, -.5, -.18);
+    //for (int i=0; i < 5; ++i)
+    //{
+	//phigone[i] = new TH1D(Form("phigone%d", i+1), "Collapsed in Phi; Z value of hit", 126, -64, 64);
+	//phigoneneg[i] = new TH1D(Form("phigoneneg%d", i+1), "Collapsed in Phi, Lesser Vals", 42, -65, -21);
+	//phigonemid[i] = new TH1D(Form("phigonemid%d", i+1), "Collapsed in Phi, Middle Vals", 42,  -21, 21);
+	//phigonepos[i] = new TH1D(Form("phigonepos%d", i+1), "Collapsed in Phi, Higher Vals;", 42,   21 ,65);
+	//zgone[i] = new TH1D(Form("zgone%d", i+1), "Collapsed in Z; Aziumthal Value of hit", 628, -3.5, 3.5); 
 	//for(int j=0; j <30; ++j)
 	//{
 	//newmods[i][j] = (new TH2D(Form("newmod%d%d",i,j), "module", 500, -200, 200, 500, -200, 200));
 	//}
-      }
+    //}
     _nEvt = 0;
     nhit = 0;
 }
@@ -140,11 +147,64 @@ void betterthanever::processEvent( LCEvent * evt )
 	double posz = hit->getPosition()[2];
 	MCParticle* particle=hit->getMCParticle();
 	int particleid = particle->getPDG();
-	if(std::find(particleids.begin(), particleids.end(), particleid) == particleids.end())                
+	double momz = particle->getMomentum()[2];
+	ogxyplane->Fill(posx, posy);
+	double radval = sqrt((posx)*(posx)+(posy)*(posy));
+	//double azimuth = atan2(posy, posx);
+	//avgradvals[layer-1]+=radval;
+	if (radval > 12.0 && radval < 16.0)
+	  {
+	    trimmedogplane->Fill(posx,posy);
+	    rsuba[layer-1][module][0]+=posx;
+	    rsuba[layer-1][module][1]+=posy;
+	    nhitlayer[layer-1]++;
+	    double azimuth = atan2(posy, posx);
+
+	  }
+	if (radval > 20.9 && radval < 24.99)
+	  {
+	    trimmedogplane->Fill(posx,posy);
+	    rsuba[layer-1][module][0]+=posx;
+	    rsuba[layer-1][module][1]+=posy;
+	    nhitlayer[layer-1]++;
+	    double azimuth = atan2(posy, posx);
+
+	  }
+	if (radval > 33.0 && radval < 37.75)
+	  {
+	    trimmedogplane->Fill(posx,posy);
+	    rsuba[layer-1][module][0]+=posx;
+	    rsuba[layer-1][module][1]+=posy;
+	    nhitlayer[layer-1]++;
+	    double azimuth = atan2(posy, posx);
+
+	  }
+	if (radval > 45.0 && radval < 50.3)
+	  {
+	    trimmedogplane->Fill(posx,posy);
+	    rsuba[layer-1][module][0]+=posx;
+	    rsuba[layer-1][module][1]+=posy;
+	    nhitlayer[layer-1]++;
+	    double azimuth = atan2(posy, posx);
+
+	  }
+	if (radval > 57.6 && radval < 63.7)
+	  {
+	    trimmedogplane->Fill(posx,posy);
+	    rsuba[layer-1][module][0]+=posx;
+	    rsuba[layer-1][module][1]+=posy;
+	    nhitlayer[layer-1]++;
+	    double azimuth = atan2(posy, posx);
+
+	  }	    
+	  
+	//zgone[layer-1]->Fill(azimuth);
+	//modhits[layer-1][module]++;
+	//layerhits[layer-1]++;
+	/*if(std::find(particleids.begin(), particleids.end(), particleid) == particleids.end())                
 	  {
 	    particleids.push_back(particleid);
 	  }
-	double momz = particle->getMomentum()[2];
 	if (momz < -.18)
 	  {
 	    negmomzvals->Fill(momz);
@@ -152,13 +212,10 @@ void betterthanever::processEvent( LCEvent * evt )
 	if (momz > 0.18)
 	  {
 	    posmomzvals->Fill(momz);
-	  }
-	++nhit;
-       	ogxyplane->Fill(posx, posy);
-	rsuba[layer-1][module][0]+=posx;
-	rsuba[layer-1][module][1]+=posy;
+	    }*/
+       	//ogxyplane->Fill(posx, posy);
 	//rsuba[layer][module][2]++;
-	phigone[layer-1]->Fill(posz);
+	/*phigone[layer-1]->Fill(posz);
 	if (posz <= -21)
 	  {
 	  phigoneneg[layer-1]->Fill(posz);
@@ -170,25 +227,16 @@ void betterthanever::processEvent( LCEvent * evt )
 	if (posz > 21)
 	  {
 	    phigonepos[layer-1]->Fill(posz);
-	  }
+	    }*/
       }
-    /*for(int i=0; i < barrelhits->getNumberOfElements(); ++i)  //loop for each hit in the collection
-      {
-        SimTrackerHit* hit = dynamic_cast<SimTrackerHit*>(barrelhits->getElementAt(i));
-        CellIDDecoder<SimTrackerHit> idDec(barrelhits);
-        int layer = idDec( hit )[ILDCellID0::layer];   //info about hits stored in these types 
-	int module = idDec (hit)[ILDCellID0::module];
-      }
-
-
-        for (int i=0; i < 5; ++i)
+    for (int i=0; i < 5; ++i)
       {                                           
 	for (int j = 0; j < 30; ++j)  
-        { 
-          rsuba[i][j][0]/=nhit;   
-          rsuba[i][j][1]/=nhit;
-          thetas[i][j][0] = atan2(rsuba[i][j][0], rsuba[i][j][1]);       
-	}                                     
+	  { 
+	    //rsuba[i][j][0]/=modhits[i][j];
+	    //rsuba[i][j][1]/=modhits[i][j];
+	    thetas[i][j][0] = atan(rsuba[i][j][0]/rsuba[i][j][1]);
+	  }                                     
       }
     for(int i=0; i < barrelhits->getNumberOfElements(); ++i)
       {
@@ -199,11 +247,46 @@ void betterthanever::processEvent( LCEvent * evt )
 	double x = hit->getPosition()[0];
 	double y = hit->getPosition()[1];
 	double z = hit->getPosition()[2];
-	double newx = ((x * cos(thetas[layer-1][module][0])) - (y * sin(thetas[layer-1][module][0])));
-	double newy = ((y * cos(thetas[layer-1][module][0])) + (x * cos(thetas[layer-1][module][0])));
-	newmods[layer-1][module]->Fill(newx, newy);
-	newxyplane->Fill(newx, newy);
-	}*/
+	double radval = sqrt((x)*(x)+(y)*(y));
+	if (radval > 12.0 && radval < 16.0)
+	  {
+	    double newx = ((x * cos(thetas[layer-1][module][0])) - (y * sin(thetas[layer-1][module][0])));
+	    double newy = ((y * cos(thetas[layer-1][module][0])) + (x * sin(thetas[layer-1][module][0])));
+	    //newmods[layer-1][module]->Fill(newx, newy);
+	    newxyplane->Fill(newx, newy);
+	  }
+	if (radval > 20.9 && radval < 24.99)
+          {
+            double newx = ((x * cos(thetas[layer-1][module][0])) - (y * sin(thetas[layer-1][module][0])));
+            double newy = ((y * cos(thetas[layer-1][module][0])) + (x * sin(thetas[layer-1][module][0])));
+            //newmods[layer-1][module]->Fill(newx, newy);
+            newxyplane->Fill(newx, newy);
+          }
+
+	if (radval > 33.0 && radval < 37.75)
+          {
+            double newx = ((x * cos(thetas[layer-1][module][0])) - (y * sin(thetas[layer-1][module][0])));
+            double newy = ((y * cos(thetas[layer-1][module][0])) + (x * sin(thetas[layer-1][module][0])));
+            //newmods[layer-1][module]->Fill(newx, newy);
+            newxyplane->Fill(newx, newy);
+          }
+
+	if (radval > 45.0 && radval < 50.3)
+          {
+            double newx = ((x * cos(thetas[layer-1][module][0])) - (y * sin(thetas[layer-1][module][0])));
+            double newy = ((y * cos(thetas[layer-1][module][0])) + (x * sin(thetas[layer-1][module][0])));
+            //newmods[layer-1][module]->Fill(newx, newy);
+            newxyplane->Fill(newx, newy);
+          }
+
+	if (radval > 57.6 && radval < 63.7)
+          {
+            double newx = ((x * cos(thetas[layer-1][module][0])) - (y * sin(thetas[layer-1][module][0])));
+            double newy = ((y * cos(thetas[layer-1][module][0])) + (x * sin(thetas[layer-1][module][0])));
+            //newmods[layer-1][module]->Fill(newx, newy);
+	    newxyplane->Fill(newx, newy);
+          }
+	}
 }
 
 
@@ -216,27 +299,23 @@ void betterthanever::check( LCEvent * evt )
 
 void betterthanever::end()
 {
+  nhit = nhitlayer[0] + nhitlayer[1] + nhitlayer[2] + nhitlayer[3] + nhitlayer[4];
   cout << "nhit: " << nhit << endl;
   cout << "_nEvt: " << _nEvt << endl;
   /*for (int i=0; i < 5; ++i)
     {
+      cout << "hits in layer " << i+1 << ": " << layerhits[i] << endl; 
       for (int j = 0; j < 30; ++j)
 	{
-	  rsuba[i][j][0]/=nhit;
-	  rsuba[i][j][1]/=nhit;
-	  thetas[i][j][0] = atan2(rsuba[i][j][0], rsuba[i][j][1]);
-	  cout << "layer: " << i << ", module: " << j <<", x-val: "
-	       << rsuba[i][j][0] << ", y-val: " << rsuba[i][j][1]
-	       << ", theta val:" <<thetas[i][j][0]; 
-	  //cout << ", z-val: " << rsuba[i][j][2];
-	  cout << endl;
+	  // cout << "layer: " << i+1 << " module: " << j+1 << " number of hits: " << modhits[i][j];
+	  //cout << endl;
 	}
       cout << endl;
-  }*/
-  /*for (int i = 0; i < particleids.size(); ++i)
-    {
-      cout << particleids[i] << endl;
       }*/
+  for (int i = 0; i < 5; ++i)
+    {
+      cout << endl;
+    }
   _rootfile->Write();
 
 }
